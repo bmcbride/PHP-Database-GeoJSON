0f723d0e73ec9f44b6f96285b36cf97621051ea7
Bryan McBride <mcbride.bryan@gmail.com>
2012-10-10 11:22:26 -0300

    Include JSON_NUMERIC_CHECK option

--- demo/bikeRacks_geojson.php
+++ demo/bikeRacks_geojson.php
@@ -49,6 +49,6 @@ while ($row = $rs->fetch(PDO::FETCH_ASSOC)) {
 }
 
 header('Content-type: application/json');
-echo json_encode($geojson);
+echo json_encode($geojson, JSON_NUMERIC_CHECK);
 $conn = NULL;
 ?>
